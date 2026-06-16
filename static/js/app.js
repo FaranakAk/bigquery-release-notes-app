00845725da4b174bf51fcff5e26aa3b2a62dfcc6
Faranak <faranak.akbarifar@gmail.com>
2026-06-15 21:56:11 -0400

    feat: group release notes chronologically with daily date headers

--- static/js/app.js
+++ static/js/app.js
@@ -281,7 +281,23 @@ function renderNotes() {
     dom.emptyView.classList.add('hidden');
     dom.notesGrid.classList.remove('hidden');
     
+    let lastDate = null;
+    
     state.filteredNotes.forEach(note => {
+        // Inject date header when the date changes
+        if (note.date !== lastDate) {
+            lastDate = note.date;
+            const dateHeader = document.createElement('h2');
+            dateHeader.className = 'date-header';
+            dateHeader.innerHTML = `
+                <svg class="date-header-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M19 4h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V10h14v10zm0-12H5V6h14v2zm-7 5h5v5h-5z"/>
+                </svg>
+                <span>${note.date}</span>
+            `;
+            dom.notesGrid.appendChild(dateHeader);
+        }
+        
         const card = document.createElement('div');
         const cleanType = note.type.toLowerCase();
         
@@ -336,7 +352,6 @@ function renderNotes() {
             <div class="card-header-row">
                 <div class="card-badge-date">
                     <span class="badge ${badgeClass}">${note.type}</span>
-                    <span class="card-date">${note.date}</span>
                 </div>
                 <div class="card-selection-controls">
                     <div class="checkbox-mock" aria-label="Select update">
